0afc4f3e0af53a20c8ebb4caf3f9c521977ff933
sindy <sindy@SINDY_ARCE>
2022-09-17 23:51:27 -0600

    Agregar archivos de proyecto.

---
+++ Division_Propiedades/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Division_Propiedades
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDivi_Click(object sender, EventArgs e)
+        {
+            //Objeto
+            cldivi pDiv = new cldivi();
+            pDiv.Nume1 = float.Parse(txtNum1.Text);
+            pDiv.Nume2 = float.Parse(txtNum2.Text);
+            textBox3.Text = pDiv.division().ToString();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            txtNum1.Clear();
+            txtNum2.Clear();
+            textBox3.Clear();
+        }
+    }
+}
---
+++ Division_Propiedades/cldivi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Division_Propiedades
+{
+    class cldivi
+    {
+        //Atributos
+        private float num1;
+        private float num2;
+
+        //Propiedades
+        public float Nume1
+        {
+            get => num1; set => num1 = value;
+        }
+        public float Nume2
+        {
+            get => num2; set => num2 = value;
+        }
+
+        //Método
+        public float division()
+        {
+            return (num1 / num2);
+        }
+    }
+}
